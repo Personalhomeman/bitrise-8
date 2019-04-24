5603293cff8e6684fe026cbe14b1ff9c8b9f01b4
Ben Martel <13800255+benmartel@users.noreply.github.com>
2019-04-24 11:29:40 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 
-CacheServerIP=$( assetcachelocatorutil 2>&1 | awk 'FNR == 3 {print}' | awk '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}' )
-
-CacheServerIPP=$CacheServerIP |  awk -F"." '{print $2}'
-
-if  [[ $CacheServerIPP = "146" || $CacheServerIPP = "156" ]]; then
-   echo 'Apple Traffic Succesful'
- else
-   echo "Fail"
-fi
+echo "hello world"
